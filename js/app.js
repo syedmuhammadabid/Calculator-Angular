8ad81b80bab76ff2adb88c8c3430745d25f4a09c
Syed Muhammad Abid <syedmuhammadabid110@gmail.com>
2017-08-07 20:24:20 +0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,37 @@
+var app = angular.module('myApp', []);
+
+app.controller('myCtrl', function ($scope) {
+
+    $scope.output = "0";
+    $scope.inOperation = false;
+    $scope.num1 = 0;
+
+    $scope.updateOutput = function (btn) {
+        if ($scope.output == "0") {
+            $scope.output = btn;
+        } else {
+            $scope.output += String(btn);
+        }
+    };
+
+    $scope.operate = function (op) {
+        if ($scope.output && !$scope.inOperation) {
+            $scope.num1 = $scope.output;
+            $scope.output += op;
+            $scope.inOperation = true;
+        } else if ($scope.output.length > $scope.num1.length + 1) {
+            $scope.output = eval($scope.output);
+            $scope.output += op;
+        }
+    };
+
+    $scope.equal = function () {
+        if ($scope.output.length > $scope.num1.length + 1) {
+            $scope.output = eval($scope.output);
+            $scope.num1 = $scope.output;
+        } else {
+            $scope.output = $scope.num1;
+        }
+    };
+
+});
